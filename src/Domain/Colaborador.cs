3cb7d509f6d773fe257a605f9dfe8aebabbc62a5
Bryan <bryanlima200@outlook.com>
2022-02-15 23:46:44 -0300

    Adicionado entidades Departamento e Colaborador

---
+++ src/Domain/Colaborador.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EFCore.UowRepository.Domain
+{
+    public class Colaborador
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+
+        public int DepartamentoId { get; set; }
+        public Departamento Departamento { get; set; }
+    }
+}
---
+++ src/Domain/Departamento.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EFCore.UowRepository.Domain
+{
+    public class Departamento
+    {
+        public int Id { get; set; }
+        public string Descricao { get; set; }
+
+        public List<Colaborador> Colaboradores { get; set; }
+    }
+}
