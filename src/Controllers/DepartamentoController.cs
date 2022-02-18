cb3fa2cb59ab9dc2fb1a8ed8716e2d058031a8bc
Bryan <bryanlima200@outlook.com>
2022-02-17 21:23:45 -0300

    Adicionado controller Departamento

---
+++ src/Controllers/DepartamentoController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EFCore.UowRepository.Data.Repositories;
+using EFCore.UowRepository.Domain;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace EFCore.UowRepository.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class DepartamentoController : ControllerBase
+    {
+        private readonly ILogger<DepartamentoController> _logger;
+        private readonly IDepartamentoRepository _departamentoRepository;
+
+        public DepartamentoController(ILogger<DepartamentoController> logger, IDepartamentoRepository departamentoRepository)
+        {
+            _logger = logger;
+            _departamentoRepository = departamentoRepository;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetByIdAsync(int id/*, [FromServices] IDepartamentoRepository repository*/)
+        {
+            //var departamento = await repository.GetByIdAsync(id);
+            var departamento = await _departamentoRepository.GetByIdAsync(id);
+
+            return Ok(departamento);
+        }
+    }
+}
