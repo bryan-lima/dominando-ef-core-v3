ae025fd81a5f8b857d0b5b3e87410ba668d9e913
Bryan <bryanlima200@outlook.com>
2022-02-17 21:34:39 -0300

    Injetado dependência do Newtonsoft em Startup

--- src/Startup.cs
+++ src/Startup.cs
@@ -29,7 +29,12 @@ namespace EFCore.UowRepository
 
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
+            services.AddControllers()
+                    .AddNewtonsoftJson(options => 
+                    {
+                        options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+                    });
+
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "EFCore.UowRepository", Version = "v1" });
