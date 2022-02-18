874e4e3a4daa39bc035aa0a3931232dc2ac6874f
Bryan <bryanlima200@outlook.com>
2022-02-17 21:22:43 -0300

    Adicionado interface IDepartamentoRepository e classe DepartamentoRepository

---
+++ src/Data/Repositories/DepartamentoRepository.cs
@@ -0,0 +1,35 @@
+﻿using EFCore.UowRepository.Domain;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EFCore.UowRepository.Data.Repositories
+{
+    public class DepartamentoRepository : IDepartamentoRepository
+    {
+        private readonly ApplicationContext _context;
+
+        public DepartamentoRepository(ApplicationContext context)
+        {
+            _context = context;
+        }
+
+        public void Add(Departamento departamento)
+        {
+            _context.Departamentos.Add(departamento);
+        }
+
+        public async Task<Departamento> GetByIdAsync(int id)
+        {
+            return await _context.Departamentos.Include(departamento => departamento.Colaboradores)
+                                               .FirstOrDefaultAsync(departamento => departamento.Id == id);
+        }
+
+        public bool Save()
+        {
+            return _context.SaveChanges() > 0;
+        }
+    }
+}
