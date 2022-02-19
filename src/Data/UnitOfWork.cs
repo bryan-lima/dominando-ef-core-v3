1f386b2939e39c9627042d8d77aaf660c5f74871
Bryan <bryanlima200@outlook.com>
2022-02-18 21:14:36 -0300

    Adicionado interface e classe UnitOfWork

---
+++ src/Data/UnitOfWork.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EFCore.UowRepository.Data
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly ApplicationContext _context;
+
+        public UnitOfWork(ApplicationContext context)
+        {
+            _context = context;
+        }
+
+        public bool Commit()
+        {
+            return _context.SaveChanges() > 0;
+        }
+
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+    }
+}
